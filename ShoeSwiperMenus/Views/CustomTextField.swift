4f91dc68bcba3d818ca1d181c8430350bd90860d
antoineneidecker <antoine.neidecker@gmail.com>
2020-03-26 18:19:08 +0100

    Fixed bug with home buttons not being able to unwrap. Also reimplemented the login and signin screens. Implemented keyboard animation with screen.

---
+++ ShoeSwiperMenus/Views/CustomTextField.swift
@@ -0,0 +1,21 @@
+//
+//  CustomTextField.swift
+//  ShoeSwiperMenus
+//
+//  Created by Antoine Neidecker on 25/03/2020.
+//  Copyright © 2020 Antoine Neidecker. All rights reserved.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
