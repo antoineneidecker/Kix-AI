6c4533eb07e9ca28a1ecbded2b336e949503bad5
antoineneidecker <antoine.neidecker@gmail.com>
2020-10-22 08:47:26 +0200

    Everything works, little tags are now on cards. Would like to make an instagram feed

---
+++ KixApp/ViewControllers/LikedPage/Feed/FeedController.swift
@@ -0,0 +1,9 @@
+//
+//  FeedController.swift
+//  ShoeSwiperMenus
+//
+//  Created by Antoine Neidecker on 15/10/2020.
+//  Copyright © 2020 Antoine Neidecker. All rights reserved.
+//
+
+import Foundation
